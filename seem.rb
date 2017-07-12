e453eac9806affa4a316bdac297336d145ff6fdc
AHN HOJUNG <nb90407@navercorp.com>
2017-07-12 19:01:17 +0900

    코드 구조및 순서 변경

--- seem.rb
+++ seem.rb
@@ -1,74 +1,25 @@
 #!/usr/bin/ruby
 module Seem
     
-    class Text
-        attr_reader :text, :path
-        
-        def self.read path
-            init_result = nil
-            File.open(path,'r') do |file|
-                init_result = self.new(file.read,path)
-            end
-            init_result
-        end
- 
-        def initialize text="", path=nil
-            @text = text
-            @path = path
-        end
-        
-        def clone
-            self.class.new(@text, @path)
-        end
-        
-        def pathExsist?
-            
-        end
-    end
-    
-    class Files
-        attr_accessor :texts
-        
-        def initialize pathes=[]
-            pathes = Seem::to_a(pathes)
-            @texts = pathes.map do |path|
-                Seem::Text.read(path)
-            end
-        end
-        
-        def each_replace exp, *iexps
-            exps = (iexps == nil ? [] : (iexps.class == Array ? iexps : [iexps])).reverse
-            
-            @texts.each do |seem_text|
-                seem_text.text.match()
-            end
-        end
-        
-        def clone
-            files_clone = self.class.new
-            files_clone.texts = @texts.map do |seem_text|
-                seem_text.clone
-            end
-            files_clone
-        end
-    end
+    @@base_path = Dir.pwd
     
     class BlockMatch
-        attr_reader :reference, :match
+        attr_reader :reference, :matches
         
         def initialize opts={}
-            @match  = {}
+            opts  = opts.class == Hash ? opts : {}
             
-            opts = opts.class == Hash ? opts : {}
-            
-            @reference,
-            @match[:head],       @match[:body],    @match[:foot],
-            @match[:depth],      @match[:begin],   @match[:end],
-            @match[:body_begin], @match[:body_end] = 
-            opts[:reference]  || nil, 
-            opts[:head]       || '',  opts[:body]     || '' , opts[:foot] || '',
-            opts[:depth]      || nil, opts[:begin]    || nil, opts[:end] || nil,
-            opts[:body_begin] || nil, opts[:body_end] || nil
+            @reference = opts[:reference]  || nil
+            @matches = [{
+                head:       opts[:head]       || '',
+                body:       opts[:body]       || '' ,
+                foot:       opts[:foot]       || '',
+                depth:      opts[:depth]      || nil,
+                begin:      opts[:begin]      || nil,
+                end:        opts[:end]        || nil,
+                body_begin: opts[:body_begin] || nil,
+                body_end:   opts[:body_end]   || nil
+            }]
         end
         def head
             @match[:head]
@@ -79,35 +30,13 @@ module Seem
         def foot
             @match[:foot]
         end
+        def matched
+            @matches.last
+        end
         def content
-            self.head + self.body + self.foot
+            last = self.matched
+            last and (last[:head] + last[:body] + last[:foot])
         end
-    end
-    
-    @@base_path = Dir.pwd
-    
-    def self.read  path, charset=nil
-        self::Text.read(path, charset=nil)
-    end
-    
-    def self.glob (pathes, base_path=@@base_path)
-        glob_pathes = []
-        
-        Seem::to_a(pathes).each do |path|
-            expand_path = File.expand_path(path,base_path);
-            expand_glob = Dir.glob(expand_path)
-            glob_pathes = glob_pathes + expand_glob if expand_glob.any?
-        end
-        
-        self::Files.new(glob_pathes)
-    end
-    
-    def self._style_block
-        [/[\w+\s^\n\}]\{/,'}']
-    end
-    
-    def self._style attr=nil
-        []
     end
     
     def self.block_matches text, exp, opt=nil
@@ -174,8 +103,7 @@ module Seem
         if track_currected
             
             body_content = text[master_header[:end] ... master_footer[:begin]];
-            
-            block_matches << Seem::BlockMatch.new({
+            hash_result  = {
                 reference:  meta[:reference],
                 head:       text[master_header[:begin] ... master_header[:end]],
                 body:       body_content,
@@ -185,7 +113,9 @@ module Seem
                 end:        meta[:offset] + master_footer[:end],
                 body_begin: meta[:offset] + master_header[:end],
                 body_end:   meta[:offset] + master_footer[:begin],
-            })
+            }
+            
+            block_matches << (meta[:plain] ? hash_result : Seem::BlockMatch.new(hash_result))
             
             # find child
             if meta[:nested]
@@ -211,6 +141,95 @@ module Seem
         
         return block_matches
     end
+    
+    
+    class Matches
+        attr_reader :reference, :matches
+        
+        def initialize text
+            @reference = text
+        end
+        def
+    end
+    
+    
+    def self.read  path, charset=nil
+        self::Text.read(path, charset=nil)
+    end
+    
+    def self.glob (pathes, base_path=@@base_path)
+        glob_pathes = []
+        
+        Seem::to_a(pathes).each do |path|
+            expand_path = File.expand_path(path,base_path);
+            expand_glob = Dir.glob(expand_path)
+            glob_pathes = glob_pathes + expand_glob if expand_glob.any?
+        end
+        
+        self::Files.new(glob_pathes)
+    end
+    
+    class Text
+        attr_reader :text, :path
+        
+        def self.read path
+            init_result = nil
+            File.open(path,'r') do |file|
+                init_result = self.new(file.read,path)
+            end
+            init_result
+        end
+ 
+        def initialize text="", path=nil
+            @text = text
+            @path = path
+        end
+        
+        def clone
+            self.class.new(@text, @path)
+        end
+        
+        def pathExsist?
+            
+        end
+    end
+    
+    class Files
+        attr_accessor :texts
+        
+        def initialize pathes=[]
+            pathes = Seem::to_a(pathes)
+            @texts = pathes.map do |path|
+                Seem::Text.read(path)
+            end
+        end
+        
+        def each_replace exp, *iexps
+            exps = (iexps == nil ? [] : (iexps.class == Array ? iexps : [iexps])).reverse
+            
+            @texts.each do |seem_text|
+                seem_text.text.match()
+            end
+        end
+        
+        def clone
+            files_clone = self.class.new
+            files_clone.texts = @texts.map do |seem_text|
+                seem_text.clone
+            end
+            files_clone
+        end
+    end
+    
+    def self._style_block
+        [/[\w+\s^\n\}]\{/,'}']
+    end
+    
+    def self._style attr=nil
+        []
+    end
+    
+    
     
     private
     
